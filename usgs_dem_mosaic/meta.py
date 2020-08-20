b9494b23aa538906dba35a1d61d392e802f7ef0e
Kyle Barron <kylebarron2@gmail.com>
2020-08-19 23:29:14 -0600

    Include WGS84 bounding box from XML meta

--- usgs_dem_mosaic/meta.py
+++ usgs_dem_mosaic/meta.py
@@ -8,11 +8,16 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def parse_xml(xml, fields=['utmzone']):
+def parse_xml(xml, fields):
     soup = BeautifulSoup(xml)
 
     # Field names must be unique within the FGDC metadata
-    return {field: soup.find(field).text for field in fields}
+    data = {}
+    for field in fields:
+        xml_field = soup.find(field)
+        data[field] = xml_field and xml_field.text
+
+    return data
 
 
 def parse_meta(meta_dir):
@@ -28,7 +33,9 @@ def parse_meta(meta_dir):
         project_dir = meta_file.parents[1].name
 
         with meta_file.open() as f:
-            d = parse_xml(f.read(), fields=['utmzone'])
+            d = parse_xml(
+                f.read(),
+                fields=['utmzone', 'westbc', 'eastbc', 'northbc', 'southbc'])
 
         d['name'] = name
         d['x'] = x
@@ -38,8 +45,7 @@ def parse_meta(meta_dir):
         data.append(d)
 
         i += 1
-
-        if i == 1000:
-            break
+        if i % 1000 == 0:
+            print(i)
 
     return pd.DataFrame(data)
