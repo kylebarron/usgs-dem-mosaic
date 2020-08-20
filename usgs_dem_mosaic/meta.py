84d7c304e0f059015e36ac6a7a8fd3e537b5dca7
Kyle Barron <kylebarron2@gmail.com>
2020-08-19 23:29:34 -0600

    Convert meta UTM boxes to WGS84

--- usgs_dem_mosaic/meta.py
+++ usgs_dem_mosaic/meta.py
@@ -4,8 +4,10 @@ Parse FGDC metadata
 import re
 from pathlib import Path
 
+import geopandas as gpd
 import pandas as pd
 from bs4 import BeautifulSoup
+from shapely.geometry import box
 
 
 def parse_xml(xml, fields):
@@ -49,3 +51,37 @@ def parse_meta(meta_dir):
             print(i)
 
     return pd.DataFrame(data)
+
+
+def meta_to_wgs84(df):
+    """Convert meta file with multiple UTM zones to WGS84
+
+    TODO check that projected position is near WGS bounds from metadata file. At
+    least a couple occurences of elevation in the ocean. Probably should be utm
+    11 instead of 10.
+    """
+    df['x'] = pd.to_numeric(df['x'])
+    df['y'] = pd.to_numeric(df['y'])
+
+    wgs84_dfs = []
+    for utmzone, group in df.groupby('utmzone'):
+        group['geometry'] = group.apply(
+            lambda row: create_box(row['x'], row['y']), axis=1)
+        utm_epsg = f'epsg:269{utmzone}'
+        group_gdf = gpd.GeoDataFrame(group, crs=utm_epsg)
+
+        wgs84 = group_gdf.to_crs(epsg=4326)
+        wgs84_dfs.append(wgs84)
+
+    concat_gdf = pd.concat(wgs84_dfs)
+    return concat_gdf
+
+
+def create_box(x, y):
+    """Create UTM shapely geometry from top left corner
+    """
+    minx = x * 10000
+    miny = (y - 1) * 10000
+    maxx = (x + 1) * 10000
+    maxy = y * 10000
+    return box(minx, miny, maxx, maxy)
